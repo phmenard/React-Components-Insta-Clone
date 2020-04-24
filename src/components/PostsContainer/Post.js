1bfd334e60097576a075a594007ac0c5bd9d350a
unknown <clmonitor2013@gmail.com>
2020-04-24 01:37:07 -0400

    first-commit

--- src/components/PostsContainer/Post.js
+++ src/components/PostsContainer/Post.js
@@ -1,15 +1,20 @@
 // You will add code in this file
-import React from "react";
+import React, { useState } from "react";
 import CommentSection from "../CommentSection/CommentSectionContainer";
 import LikeSection from "./LikeSection";
 import PostHeader from "./PostHeader";
 
 import "./Posts.css";
 
+
+
+
 // pass props in this file to
 const Post = props => {
   // set up state for the likes
+  const [likes, setLikes] = useState(15);
 
+  
   return (
     <div className="post-border">
       <PostHeader
@@ -25,7 +30,7 @@ const Post = props => {
           src={props.post.imageUrl}
         />
       </div>
-      <LikeSection />
+      <LikeSection likes={likes}/>
       <CommentSection
         postId={props.post.imageUrl}
         comments={props.post.comments}
